041a9c6b712783382446008d9444e860b46006be
yan <2268389851@qq.com>
2023-12-25 14:47:56 +0800

    弹窗的字段问题

--- application/popups/controller/Popups.php
+++ application/popups/controller/Popups.php
@@ -8,6 +8,7 @@ use think\App;
 use think\Controller;
 use think\Db;
 use think\Request;
+use DateTime;
 
 class Popups extends Controller
 {
@@ -57,19 +58,39 @@ class Popups extends Controller
      */
     public function save(Request $request)
     {
-        $data=[
-            'start_date'    => $request->param('start_date',null),
-            'end_date'      => $request->param('end_date',null),
-            'content'       => $request->param('content',null),
-            'title'         => $request->param('title',null),
-            'image'         => $request->param('image',null)
+        $start_date = $request->param('start_date', null);
+        $end_date = $request->param('end_date', null);
+        $content = $request->param('content', null);
+        $title = $request->param('title', null);
+        $image = $request->param('image', null);
+
+        // 检查开始时间是否小于结束时间
+        if (!empty($start_date) && !empty($end_date)) {
+            $startDateTime = new DateTime($start_date);
+            $endDateTime = new DateTime($end_date);
+
+            if ($startDateTime >= $endDateTime) {
+                echoJson(0, '开始时间必须小于结束时间');
+                return;
+            }
+        }
+
+        $data = [
+            'start_date' => $start_date,
+            'end_date' => $end_date,
+            'content' => $content,
+            'title' => $title,
+            'image' => $image
         ];
-        $validate=new Popupsv();
-        if(!$validate->check($data)){
-            echoJson('0',$validate->getError());
+
+        $validate = new Popupsv();
+        if (!$validate->check($data)) {
+            echoJson(0, $validate->getError());
+            return;
         }
 
         $this->popups->addpop($data);
+        // 如果以上条件都满足，执行保存逻辑
     }
 
     /**
@@ -153,19 +174,27 @@ class Popups extends Controller
      */
     public function update(Request $request, $id)
     {
-        $date = $request->param('start_date',null);
-        $data = [];
+
+        $currentDate = new DateTime();
+        $currentDate->setTime(0, 0, 0);
+
+        $date = $request->param('end_date', null);
 
         if (!empty($date)) {
-            if (strtotime($date) <= time()) {
-                echoJson(0, '更新的时间必须大于当前时间');
+            $givenDate = new DateTime($date);
+            $givenDate->setTime(0, 0, 0);
+
+            if ($givenDate < $currentDate) {
+                echoJson(0, '更新的时间必须大于等于当前时间');
             }
-            $data['date'] = $date;
+
+            $data['end_date'] = $date;
         }
+
 
         $content = $request->param('content', null);
 
-        if(!empty($data)) {
+        if(!empty($content)) {
             $data['content'] = $content;
         }
 
@@ -181,7 +210,7 @@ class Popups extends Controller
             $data['image'] = $image;
         }
 
-        Db::table('send_message')->where('id', $id)->insert($data);
+        Db::table('send_message')->where('id', $id)->update($data);
         echoJson(1,'更新成功');
     }
 
