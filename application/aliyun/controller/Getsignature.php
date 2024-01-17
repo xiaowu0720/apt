898119531b28fe35cefc738b90943e8d70f56987
xiaowutongxue123 <2268389851@qq.com>
2024-01-17 21:20:58 +0800

    test

--- application/aliyun/controller/Getsignature.php
+++ application/aliyun/controller/Getsignature.php
@@ -10,7 +10,7 @@ class Getsignature extends Controller
     public function getOssSignature(\app\aliyun\model\Getsignture_model $oss){
         if ($this->request->isGet())
         {
-            $road = input("road");
+            $road = 'test';
             echoJson(1,"生成阿里云签名成功", $oss->getSignature($road));
         }
     }
